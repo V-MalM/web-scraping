09f11c72d93e09649c693322e937501e0d08d569
V-MalM <81383838+V-MalM@users.noreply.github.com>
2021-08-13 14:20:32 -0500

    Saving app and scrape code

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect
+import pymongo
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data"
+# mongo = PyMongo(app)
+
+# Kevin Code
+conn_url = "mongodb://localhost:27017"
+mongo =  pymongo.MongoClient(conn_url)
+
+# Declare the database
+db = mongo.mars_data
+
+# Declare the collection
+mars_data = db.mars_data
+
+
+@app.route("/")
+def index():
+    results = mars_data.find_one()
+    return render_template("index.html", results=results)
+
+
+@app.route("/scrape")
+def scraper():
+    # mars_data = mongo.db.mars_data
+    scrape_data = scrape_mars.scrape()
+    mars_data.update({}, scrape_data, upsert=True)
+    #return(scrape_data)
+    return redirect("/", code=302)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
