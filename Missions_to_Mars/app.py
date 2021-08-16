12db2b94d03b99416b6c3d9e6b2b37e45fe65ba1
V-MalM <81383838+V-MalM@users.noreply.github.com>
2021-08-16 13:34:16 -0500

    saving app

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,6 +5,7 @@ import scrape_mars
 
 app = Flask(__name__)
 
+# Connecting to MongoDB
 conn_url = "mongodb://localhost:27017"
 mongo =  pymongo.MongoClient(conn_url)
 
@@ -18,17 +19,25 @@ mars_data = db.mars_data
 @app.route("/")
 def index():
     # Fetching records. we are only creating one record and updating it with each scrape 
-    # so this will fetch just one record in to the results list. Alternatively we can use find_one
+    # This will fetch just one record (in the form od a list) into 'results' because the collection has only one
+    # Alternatively we can use find_one.
     results = mars_data.find() 
-    
+
+    # the results are rendered and displayed in HTML 
     return render_template("index.html", results=results)
 
 
 @app.route("/scrape")
 def scraper():
 
+    # calling scrape function from scrape_mars.py 
+    # It returns scraped information in the form of a dictionary into the variable 'scrape_data'
     scrape_data = scrape_mars.scrape()
+
+    # inserting/updading mars_data collection with new scraped data
     mars_data.update_one({}, {"$set":scrape_data}, upsert=True)
+
+    # redirecting to home page
     return redirect("/", code=302)
     
 if __name__ == "__main__":
